5ee5da26c4af34e1d6b238bdbcbbeb0d9b26f050
Nguyen Tan Tri <nguyentantri.a4@gmail.com>
2024-04-26 23:09:03 +0700

    angular-fe1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { SidenavComponent } from './layout/sidenav/sidenav.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
-import { HomeComponent } from './layout/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { PluginComponent } from './layout/plugin/plugin.component';
+import { TableComponent } from './pages/table/table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidenavComponent, NavbarComponent, FooterComponent, HomeComponent, PluginComponent],
+  imports: [RouterOutlet, SidenavComponent, NavbarComponent, FooterComponent, HomeComponent, PluginComponent, TableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
   title = '22012035-app';
-  
+
 
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { TableComponent } from './pages/table/table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'table', component: TableComponent }
+];
