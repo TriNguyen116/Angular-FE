f7ba64739f3f07c152568232ff7c1bc62e784edc
Nguyen Tan Tri <nguyentantri.a4@gmail.com>
2024-04-26 14:21:15 +0700

    angular-fe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidenavComponent } from './layout/sidenav/sidenav.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './layout/home/home.component';
+import { PluginComponent } from './layout/plugin/plugin.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidenavComponent, NavbarComponent, FooterComponent, HomeComponent, PluginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = '22012035-app';
+  
+
 }
