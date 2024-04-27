73ceaf10cbddae3cf9f4f13b1f595661a5674a6f
Nguyen Tan Tri <nguyentantri.a4@gmail.com>
2024-04-27 13:36:26 +0700

    angular-fe-firebase

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, inject } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
+export interface Item { id: string; name: string; description: string; price: number; stuatus: string }
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
 export class TableComponent {
-
+  firestore: Firestore = inject(Firestore)
+  items: Observable<any[]>;
+  constructor() {
+    const aCollection = collection(this.firestore, 'items')
+    this.items = collectionData(aCollection);
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBT0CKdf72G1otFBbNo8VW4iJmTsGwtYbQ",
+    authDomain: "web-angularfe.firebaseapp.com",
+    projectId: "web-angularfe",
+    storageBucket: "web-angularfe.appspot.com",
+    messagingSenderId: "295271034170",
+    appId: "1:295271034170:web:4340078b47a0722869b403",
+    measurementId: "G-9ZZS7ES2ZM"
+  }
+
+};
